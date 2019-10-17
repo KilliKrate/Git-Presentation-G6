17d5b39720c514999479740457a14c3749454402
KilliKrate <KilliKrate@users.noreply.github.com>
2019-10-17 12:13:10 +0200

    Implementation of console text

---
+++ reveal.js/js/Ovidiu.js
@@ -0,0 +1,30 @@
+ consoleText(['git config --global user.name'], 'text',['white']);
+
+
+ /* Console Text: https://codepen.io/Tbgse/pen/dYaJyJ */
+ 
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+    if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+    } else {
+      con.className = 'console-underscore'
+      visible = true;
+    }
+  }, 400)
+}
