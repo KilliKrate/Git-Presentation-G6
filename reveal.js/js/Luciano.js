b7b50bda86559e56229fd8b539c467f005ba3555
KilliKrate <KilliKrate@users.noreply.github.com>
2019-10-30 13:36:34 +0100

    Removed gitgraph.js

--- reveal.js/js/Luciano.js
+++ reveal.js/js/Luciano.js
@@ -19,29 +19,3 @@ function writeCode(word, target, color, speed) {
 var gitSlider = simpleslider.getSlider({
     container: document.getElementById('gitSlider')
 })
-
-/* Git Graph */
-const graphContainer = document.getElementById("branchesGraph");
-
-// Instantiate the graph.
-const gitgraph = GitgraphJS.createGitgraph(graphContainer);
-
-// Simulate git commands with Gitgraph API.
-const master = gitgraph.branch("master");
-master.commit({
-    author: "Jacky <prince@dutunning.com>", subject: "Initial commit"
-});
-
-const develop = gitgraph.branch("develop");
-develop.commit("Add TypeScript");
-
-const aFeature = gitgraph.branch("a-feature");
-aFeature
-    .commit("Make it work")
-    .commit("Make it right")
-    .commit("Make it fast");
-
-develop.merge(aFeature);
-develop.commit("Prepare v1");
-
-master.merge(develop).tag("v1.0.0");
